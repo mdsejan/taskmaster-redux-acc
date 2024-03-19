d79c29c2c0bbc178924b132b6ad124361685450d
Sejan~ <md4sejan@gmail.com>
2024-03-19 14:45:30 +0600

    task form added

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,83 @@
+import { useForm } from "react-hook-form";
+import Modal from "../ui/Modal";
+
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={"Add New Task"}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-3 mt-5">
+          <label htmlFor="title">Title</label>
+          <input
+            className="w-full rounded-md"
+            type="text"
+            id="title"
+            {...register("name")}
+          />
+        </div>
+        <div className="flex flex-col gap-3 mt-5">
+          <label htmlFor="description">Description</label>
+          <textarea
+            className="form-textarea mt-1 block w-full h-24 rounded-md"
+            rows="3"
+            id="description"
+            {...register("description")}
+            placeholder="Enter some long form content."
+          ></textarea>
+        </div>
+
+        <div className="flex flex-col gap-3 mt-5">
+          <label htmlFor="deadline">Deadline</label>
+          <input
+            type="date"
+            id="deadline"
+            {...register("deadline")}
+            className="form-input  block w-full rounded-md"
+          />
+        </div>
+
+        <div className="flex flex-col gap-3 mt-5">
+          <label htmlFor="assign">Assign to</label>
+          <select
+            id="assign"
+            {...register("assign")}
+            className="block w-full rounded-md"
+          >
+            <option>Sanjida Ahmed</option>
+            <option>Farhan Khan</option>
+            <option>Sumaiya Islam</option>
+            <option>Nazmul Hasan</option>
+            <option>Tasnim Akhtar</option>
+            <option>Rezaul Karim</option>
+          </select>
+        </div>
+
+        <div className="flex flex-col gap-3 mt-5">
+          <label htmlFor="priority">Priority</label>
+          <select
+            id="priority"
+            {...register("priority")}
+            className="block w-full rounded-md"
+          >
+            <option>High</option>
+            <option>Medium</option>
+            <option>Low</option>
+          </select>
+        </div>
+
+        <button
+          className="mt-4 bg-blue-500 text-white py-2 px-6 rounded-md"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddTaskModal;
--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -36,7 +36,7 @@ export default function Modal({ isOpen, setIsOpen, title, children }) {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg leading-6 text-gray-900 text-center font-semibold"
                   >
                     {title}
                   </Dialog.Title>
